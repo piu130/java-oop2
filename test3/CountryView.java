bfbb1f34005ff768c48adc8bd9fa5687e920fb7d
Ramon Boss <ramon.boss@outlook.com>
2017-04-02 10:47:41 +0200

    splitted code

--- test3/CountryView.java
+++ test3/CountryView.java
@@ -4,11 +4,13 @@ import javafx.beans.binding.Bindings;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -31,7 +33,7 @@ public class CountryView extends Stage {
         this.show();
     }
 
-    private ListView createListView() {
+    private Node createListView() {
         ListView<Country> countryListView = new ListView<>();
         countryListView.setItems(FXCollections.observableList(model.getCountries()));
 
@@ -42,7 +44,7 @@ public class CountryView extends Stage {
         return countryListView;
     }
 
-    private TextField createInputField() {
+    private Node createInputField() {
         TextField textField = new TextField();
         textField.setPromptText("Enter #country");
         textField.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -56,50 +58,76 @@ public class CountryView extends Stage {
         return textField;
     }
 
-    private GridPane createCountryInfo() {
-        Label countryInfoName = new Label("Country(* = all):");
-        Label countryInfoPopulation = new Label("Population:");
-        Label countryInfoArea = new Label("Area:");
-        Label countryInfoPopulationDensity = new Label("Population Density:");
+    private Node createCountryInfo() {
+        return new VBox(
+            createCountryInfoName(),
+            createCountryInfoPopulation(),
+            createCountryInfoArea(),
+            createCountryInfoPopulationDensity()
+        );
+    }
 
-        Label countryInfoNameValue = new Label();
-        countryInfoNameValue.textProperty().bind(
+    private Node createCountryInfoName() {
+        Label text = new Label("Country(* = all):");
+
+        Label value = new Label();
+        value.textProperty().bind(
             Bindings.createStringBinding(() -> selectedCountry.get().getIndex() == CountriesModel.DEFAULT_COUNTRY ?
-                    model.getDefaultName() :
-                    selectedCountry.get().getName(),
+                model.getDefaultName() :
+                selectedCountry.get().getName(),
             selectedCountry)
         );
 
-        Label countryInfoPopulationValue = new Label();
-        countryInfoPopulationValue.textProperty().bind(
+        HBox hBox = new HBox(text, value);
+        hBox.setSpacing(23);
+        return hBox;
+    }
+
+    private Node createCountryInfoPopulation() {
+        Label text = new Label("Population:");
+
+        Label value = new Label();
+        value.textProperty().bind(
             Bindings.createStringBinding(() -> Long.toString(selectedCountry.get().getIndex() == CountriesModel.DEFAULT_COUNTRY ?
                 model.getTotalPopulation() :
                 selectedCountry.get().getPopulation()),
             selectedCountry)
         );
 
-        Label countryInfoAreaValue = new Label();
-        countryInfoAreaValue.textProperty().bind(
+        HBox hBox = new HBox(text, value);
+        hBox.setSpacing(45);
+        return hBox;
+    }
+
+    private Node createCountryInfoArea() {
+        Label text = new Label("Area:");
+
+        Label value = new Label();
+        value.textProperty().bind(
             Bindings.createStringBinding(() -> Long.toString(selectedCountry.get().getIndex() == CountriesModel.DEFAULT_COUNTRY ?
                 model.getTotalArea() :
                 selectedCountry.get().getArea()),
             selectedCountry)
         );
 
-        Label countryInfoPopulationDensityValue = new Label();
-        countryInfoPopulationDensityValue.textProperty().bind(
+        HBox hBox = new HBox(text, value);
+        hBox.setSpacing(79);
+        return hBox;
+    }
+
+    private Node createCountryInfoPopulationDensity() {
+        Label text = new Label("Population Density:");
+
+        Label value = new Label();
+        value.textProperty().bind(
             Bindings.createStringBinding(() -> String.format("%.3f", selectedCountry.get().getIndex() == CountriesModel.DEFAULT_COUNTRY ?
                 model.getTotalPopulationDensity() :
                 selectedCountry.get().getPopulationDensity()),
             selectedCountry)
         );
 
-        GridPane countryInfo = new GridPane();
-        countryInfo.addRow(0, countryInfoName, countryInfoNameValue);
-        countryInfo.addRow(1, countryInfoPopulation, countryInfoPopulationValue);
-        countryInfo.addRow(2, countryInfoArea, countryInfoAreaValue);
-        countryInfo.addRow(3, countryInfoPopulationDensity, countryInfoPopulationDensityValue);
-
-        return countryInfo;
+        HBox hBox = new HBox(text, value);
+        hBox.setSpacing(3);
+        return hBox;
     }
 }
